1749dd80c3ef9056a1724e26d110ce926fbedf46
VladyDADDY69 <128294026+VladyDADDY69@users.noreply.github.com>
2023-03-20 21:26:18 +0200

    PHP

---
+++ Assets/scripts/PlayerHealth1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth1 : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
