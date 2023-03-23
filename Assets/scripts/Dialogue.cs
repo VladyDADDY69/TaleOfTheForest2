99f393f811b8d27e62b04d960cae0f2772ce6891
AlexGamer28 <128294064+AlexGamer28@users.noreply.github.com>
2023-03-23 21:53:01 +0200

    La barfa cu bufnita

--- Assets/scripts/Dialogue.cs
+++ Assets/scripts/Dialogue.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Dialogue : MonoBehaviour
 {
@@ -11,17 +12,27 @@ public class Dialogue : MonoBehaviour
     public string[] dialogue;
     private int index = 0;
     public float wordSpeed;
-    
-
+    public GameObject contButton;
+    private int nextSceneToLoad;
 
     void Start()
     {
+        dialoguePanel.SetActive(true);
         StartCoroutine(Typing());
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+       if(dialogueText.text== dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+       if(index== dialogue.Length)
+        {
+            dialoguePanel.SetActive(false);
+        }
        
     }
 
@@ -35,8 +46,24 @@ public class Dialogue : MonoBehaviour
             yield return new WaitForSeconds(wordSpeed);
         }
     }
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            dialogueText.text = "";
+            contButton.SetActive(false);
+            dialoguePanel.SetActive(false);
+            SceneManager.LoadScene(nextSceneToLoad);
+        }
+    }
+    
 
-   
 
-   
 }
