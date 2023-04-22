4606d4bd12029308e2707af385ffde3e877dfd3e
VladyDADDY69 <128294026+VladyDADDY69@users.noreply.github.com>
2023-04-23 00:44:09 +0300

    writing

--- Assets/scripts/LogicScript.cs
+++ Assets/scripts/LogicScript.cs
@@ -8,7 +8,7 @@ public class LogicScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        tutorial.SetActive(false);
     }
 
     // Update is called once per frame
